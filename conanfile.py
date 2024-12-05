aebf1832130bc73e37cd52b060f134629a5679e7
Cole Richardson <cole.richardson12@gmail.com>
2024-12-05 06:23:51 -0500

    Attempt to add libpcap to conan build (but it crashes docker desktop)?!

--- conanfile.py
+++ conanfile.py
@@ -2,6 +2,7 @@ from conan import ConanFile
 
 # INCREMENT THIS COMMENT # TO REBUILD THE DEPENDENCIES IN GITHUB ACTIONS: 0
 
+
 class VideoserverRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeDeps"
@@ -9,8 +10,9 @@ class VideoserverRecipe(ConanFile):
     def requirements(self):
         self.requires("fmt/11.0.2", options={"shared": False}, force=True)
         self.requires("spdlog/1.15.0", options={"shared": False})
-        self.requires("boost/1.86.0",
-                      options={"shared": False, "without_python": True})
+        self.requires("boost/1.86.0", options={"shared": False, "without_python": True})
         self.requires("cpptrace/0.7.3", options={"shared": False})
         # Testing
         self.requires("catch2/3.7.1", options={"shared": False})
+        # Examples
+        self.requires("libpcap/1.10.4", options={"shared": False})
